fbe0494f40b5db9ff5e4233ced5f021216bb2b0c
MonikaPandian <panmonikmm@gmail.com>
2022-10-17 11:40:22 +0530

    hall-booking-api

--- routes/getAllRooms.js
+++ routes/getAllRooms.js
@@ -4,7 +4,7 @@ import { client } from "../index.js";
 const router = express.Router();
 
 router.get("/",async (request,response)=>{
-    const rooms = await client.db("hallBooking").collection("rooms").find().toArray();
+    const rooms = await client.db("hallBooking").collection("bookRooms").find().toArray();
     response.send(rooms);
 })
 
